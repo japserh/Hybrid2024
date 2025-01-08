358fa9b03e395373afb2feff2f68daf9ea5fc4d8
David de Gelder <davidosodegelder@gmail.com>
2025-01-08 13:46:40 +0100

    turn some stairs

---
+++ Assets/Scripts/RotationFollower.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+
+public class RotationFollower : MonoBehaviour
+{
+    public GameObject target;
+    public float multiplier = 1;
+    private Vector3 position;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotation = target.transform.localRotation;
+        rotation = Quaternion.SlerpUnclamped(Quaternion.identity,rotation, multiplier);
+        transform.localRotation = rotation;
+    }
+}
